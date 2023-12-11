c1c95074012d1fd4963ec99bb355b42493b0d2cf
Seth Nuzum <sethnuzum@Seths-MacBook-Pro.local>
2023-12-10 21:37:05 -0800

    Finished building, implementing and testing both algos on given test image.

--- main.py
+++ main.py
@@ -3,166 +3,172 @@
 # By Seth Nuzum and Adam Erskine
 #########################
 
-
-from PIL import Image, ImageDraw
-from collections import deque
+# All imports here:
+from PIL import Image
+import numpy as np
 import heapq
 
 
-# This one checks if a pixel is valid:
-def is_valid_pixel(pixel):
-    r, g, b = pixel
-    return r > 100 or g > 100 or b > 100
+# Here we are seeing if a pixel is valid (wall or path)
+def is_valid_pixel(pixel, threshold=(100, 100, 100)):
+    return any(channel > threshold[i] for i, channel in enumerate(pixel))
 
 
-from PIL import Image, ImageDraw
-from collections import deque
+# Red line movement, no diagonals:
+def get_neighbors_4dir(x, y, width, height):  # For BFS
+    directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]  # 4-directional
+    neighbors = []
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < width and 0 <= ny < height:
+            neighbors.append((nx, ny))
+    return neighbors
 
 
+def get_neighbors_8dir(x, y, width, height):
+    directions = [
+        (0, 1),
+        (1, 0),
+        (-1, 0),
+        (0, -1),
+        (1, 1),
+        (-1, -1),
+        (1, -1),
+        (-1, 1),
+    ]
+    neighbors = []
+    for dx, dy in directions:
+        nx, ny = x + dx, y + dy
+        if 0 <= nx < width and 0 <= ny < height:
+            neighbors.append((nx, ny))
+    return neighbors
+
+
+def heuristic(curr, goal):
+    return (
+        (curr[0] - goal[0]) ** 2 + (curr[1] - goal[1]) ** 2
+    ) ** 0.5  # Euclidean distance
+
+
+# First algo done:
 def bfs(image, start, goal):
     width, height = image.size
-    start = (start[1], start[0])
-    goal = (goal[1], goal[0])
-
-    def get_neighbors(pos):
-        x, y = pos
-        neighbors = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
-        return [
-            (nx, ny) for nx, ny in neighbors if 0 <= nx < width and 0 <= ny < height
-        ]
-
-    def is_valid_pixel(pixel):
-        r, g, b = pixel
-        return r > 100 or g > 100 or b > 100
-
-    queue = deque([start])
-    visited = {start}
-    prev = {start: None}
-
-    while queue:
-        current = queue.popleft()
-        if current == goal:
-            break
-
-        for neighbor in get_neighbors(current):
-            if neighbor not in visited and is_valid_pixel(image.getpixel(neighbor)):
-                queue.append(neighbor)
-                visited.add(neighbor)
-                prev[neighbor] = current
-
-    path_length = 0
-    path_pixels = []
-
-    # Backtrack from goal to start to find path
-    at = goal
-    while at is not None:
-        path_pixels.append(at)
-        at = prev[at]
-        path_length += 1
-
-    # Create a copy of the image to draw the path
-    output_image = image.copy()
-    draw = ImageDraw.Draw(output_image)
-
-    # Draw the path in red and visited nodes in green
-    for pixel in path_pixels:
-        draw.point(pixel, (255, 0, 0))  # Red for path
-    for pixel in visited - set(path_pixels):
-        draw.point(pixel, (0, 255, 0))  # Green for visited nodes
-
-    return path_length - 1, output_image
-
-
-def best_first_search(image, start, goal):
-    width, height = image.size
-    start = (start[1], start[0])  # Convert (row, col) to (x, y)
-    goal = (goal[1], goal[0])  # Convert (row, col) to (x, y)
-
-    def get_neighbors(pos):
-        x, y = pos
-        neighbors = [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
-        return [
-            (nx, ny) for nx, ny in neighbors if 0 <= nx < width and 0 <= ny < height
-        ]
-
-    def heuristic(pos, goal):
-        return abs(pos[0] - goal[0]) + abs(pos[1] - goal[1])
-
-    priority_queue = [(0 + heuristic(start, goal), 0, start)]
-    visited = {start}
+    queue = [start]
+    visited = set()
+    visited.add(start)
     prev = {start: None}
     distances = {start: 0}
+    path_found = False
 
-    while priority_queue:
-        _, dist, current = heapq.heappop(priority_queue)
-        if current == goal:
+    while queue:
+        x, y = queue.pop(0)
+        if (x, y) == goal:
+            path_found = True
             break
 
-        for neighbor in get_neighbors(current):
-            if neighbor not in visited and is_valid_pixel(image.getpixel(neighbor)):
-                visited.add(neighbor)
-                prev[neighbor] = current
-                distances[neighbor] = dist + 1
+        for nx, ny in get_neighbors_4dir(x, y, width, height):
+            if (nx, ny) not in visited and is_valid_pixel(image.getpixel((nx, ny))):
+                queue.append((nx, ny))
+                visited.add((nx, ny))
+                prev[(nx, ny)] = (x, y)
+                distances[(nx, ny)] = distances[(x, y)] + 1
+
+    path = []
+    if path_found:
+        curr = goal
+        while curr:
+            path.append(curr)
+            curr = prev[curr]
+        path.reverse()
+    return path, visited
+
+
+# second algo done:
+def best_first_search(image, start, goal):
+    width, height = image.size
+    queue = [(0 + heuristic(start, goal), start)]
+    visited = set()
+    visited.add(start)
+    prev = {start: None}
+    distances = {start: 0}
+    path_found = False
+
+    while queue:
+        _, (x, y) = heapq.heappop(queue)
+        if (x, y) == goal:
+            path_found = True
+            break
+
+        for nx, ny in get_neighbors_8dir(x, y, width, height):
+            if (nx, ny) not in visited and is_valid_pixel(image.getpixel((nx, ny))):
                 heapq.heappush(
-                    priority_queue,
-                    (
-                        distances[neighbor] + heuristic(neighbor, goal),
-                        distances[neighbor],
-                        neighbor,
-                    ),
+                    queue, (distances[(x, y)] + 1 + heuristic((nx, ny), goal), (nx, ny))
                 )
+                visited.add((nx, ny))
+                prev[(nx, ny)] = (x, y)
+                distances[(nx, ny)] = distances[(x, y)] + 1
 
-    path_length = 0
-    path_pixels = []
-
-    # Backtrack from goal to start to find path
-    at = goal
-    while at is not None:
-        path_pixels.append(at)
-        at = prev[at]
-        path_length += 1
-
-    # Create a copy of the image to draw the path
-    output_image = image.copy()
-    draw = ImageDraw.Draw(output_image)
-
-    # Draw the path in red and visited nodes in green
-    for pixel in path_pixels:
-        draw.point(pixel, (255, 0, 0))  # Red for path
-    for pixel in visited - set(path_pixels):
-        draw.point(pixel, (0, 255, 0))  # Green for visited nodes
-
-    return path_length - 1, output_image
+    path = []
+    if path_found:
+        curr = goal
+        while curr:
+            path.append(curr)
+            curr = prev[curr]
+        path.reverse()
+    return path, visited
 
 
-# Main Function
+def create_output_image(input_image, path, visited):
+    output_image = input_image.copy()
+    for x, y in visited:
+        output_image.putpixel((x, y), (0, 255, 0))  # green
+    for x, y in path:
+        output_image.putpixel((x, y), (255, 0, 0))  # red
+    return output_image
+
+
+# Main function to fit spec:
 def main():
-    input_image_name = input("Enter the name of the input BMP file: ")
-    start_row = int(input("Enter the start row: "))
-    start_col = int(input("Enter the start column: "))
-    goal_row = int(input("Enter the goal row: "))
-    goal_col = int(input("Enter the goal column: "))
-    output_image_name_bfs = input("Enter the name of the output BMP file for BFS: ")
-    output_image_name_bestfs = input(
-        "Enter the name of the output BMP file for BestFS: "
+    # Request input BMP file name
+    input_file = input("Enter the name of the input BMP file: ")
+
+    # Request starting vertex coordinates
+    start_coords = input(
+        "Enter the starting vertex coordinates (x, y) separated by a comma: "
+    )
+    start_x, start_y = map(int, start_coords.split(","))
+
+    # Request goal vertex coordinates
+    goal_coords = input(
+        "Enter the goal vertex coordinates (x, y) separated by a comma: "
+    )
+    goal_x, goal_y = map(int, goal_coords.split(","))
+
+    # Open the image
+    image = Image.open(input_file)
+
+    # Run BFS and Best-First Search
+    bfs_path, bfs_visited = bfs(image, (start_x, start_y), (goal_x, goal_y))
+    a_star_path, a_star_visited = best_first_search(
+        image, (start_x, start_y), (goal_x, goal_y)
     )
 
-    # Load the image
-    image = Image.open(input_image_name)
+    # Output the lengths of the shortest paths
+    print("Length of the shortest path (BFS):", len(bfs_path))
+    print("Length of the shortest path (Best-First Search):", len(a_star_path))
 
-    # Run BFS
-    bfs_path_length, bfs_image = bfs(
-        image, (start_row, start_col), (goal_row, goal_col)
+    # Request names for the output files
+    bfs_output_file = input("Enter the name of the BFS output file (BMP file): ")
+    a_star_output_file = input(
+        "Enter the name of the Best-First Search output file (BMP file): "
     )
-    bfs_image.save(output_image_name_bfs)
-    print(f"BFS Path Length: {bfs_path_length}")
 
-    # Run BestFS
-    bestfs_path_length, bestfs_image = best_first_search(
-        image, (start_row, start_col), (goal_row, goal_col)
-    )
-    bestfs_image.save(output_image_name_bestfs)
-    print(f"BestFS Path Length: {bestfs_path_length}")
+    # Create output images and save them
+    bfs_output_image = create_output_image(image, bfs_path, bfs_visited)
+    a_star_output_image = create_output_image(image, a_star_path, a_star_visited)
+
+    bfs_output_image.save(bfs_output_file)
+    a_star_output_image.save(a_star_output_file)
 
 
 if __name__ == "__main__":
